9e328eaa5c4296a6eefcb8c61d4087aa4c275baf
podyankee <podyankee@gmail.com>
2021-03-07 15:36:50 +0300

    initial commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+	return (
+		<header className='header'>
+		<img src="https://www.pinclipart.com/picdir/big/536-5368351_apple-logo-png-clipart-clipart-apple-logo-png.png" alt=""/>
+	</header>
+	);
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+const Profile = () => {
+	return (
+		<div className='content'>
+				<div>
+					<img src="https://images.unsplash.com/photo-1517042271736-ddacb7a2263a?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1500&q=80" alt=""/>
+				</div>
+				<div>
+					ava + description
+				</div>
+				<div>
+					My posts
+					<div>
+						New posts
+					</div>
+					<div>
+						<div>
+							Post 1
+						</div>
+						<div>
+							Post 2
+						</div>
+					</div>
+				</div>
+			</div>
+	);
+}
+
+export default Profile;
