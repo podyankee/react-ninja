7fc983b8fe609347846cc2fbcc41754d051d7e24
podyankee <podyankee@gmail.com>
2021-03-09 00:09:20 +0300

    Lesson 21 Dialog page

--- src/components/dialogs/Dialogs.jsx
+++ src/components/dialogs/Dialogs.jsx
@@ -1,12 +1,43 @@
 import React from 'react';
 
-// import s from './Dialog.module.sass';
+import s from './Dialog.module.sass';
 
 const Dialogs = (props) => {
 	return (
-		<div>
-			Dialogs
+		<div className={s.dialogs}>
+			<div className={s.dialogsItems}>
+				<div className={s.dialog + ' ' + s.active}>
+					Dimych
+				</div>
+				<div className={s.dialog}>
+					Andrey
+				</div>
+				<div className={s.dialog}>
+					Sveta
+				</div>
+				<div className={s.dialog}>
+					Sasha
+				</div>
+				<div className={s.dialog}>
+					Victor
+				</div>
+				<div className={s.dialog}>
+					Valera
+				</div>
+			</div>
+			<div className={s.messages}>
+				<div className={s.message}>
+					Hi
+				</div>
+				<div className={s.message}>
+					Hello
+				</div>
+				<div className={s.message}>
+					Yo
+				</div>
+			</div>
 		</div>
+
 	)
 }
 
