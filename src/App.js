e1e2267bfca90870a9b0b36c6b00041aa08a1ca5
podyankee <podyankee@gmail.com>
2021-03-08 21:43:26 +0300

    styled compponents and routing added

--- src/App.js
+++ src/App.js
@@ -1,19 +1,33 @@
 import React from 'react';
 
 import './App.css';
-import Header from './components/Header';
-import Navbar from './components/Navbar';
-import Profile from './components/Profile';
+
+import Dialogs from './components/dialogs/Dialogs';
+import Header from './components/header/Header';
+import Navbar from './components/navbar/Navbar';
+import Profile from './components/profile/Profile';
+import News from './components/news/News';
+import Music from './components/music/Music';
+import Settings from './components/settings/Settings';
+import {BrowserRouter, Route} from 'react-router-dom';
 
 
 
 const App = () =>{
   return (
-		<div className='app-wrapper'>
-			<Header />
-			<Navbar />
-			<Profile />
-		</div>
+		<BrowserRouter>
+			<div className='app-wrapper'>
+				<Header />
+				<Navbar />
+				<div className='app-wrapper-content'>
+					<Route path='/dialogs' component={Dialogs} />
+					<Route path='/profile' component={Profile} />
+					<Route path='/news' component={News} />
+					<Route path='/music' component={Music} />
+					<Route path='/settings' component={Settings} />
+				</div>
+			</div>
+		</BrowserRouter>
   );
 }
 
--- src/components/profile/Profile.jsx
+++ src/components/profile/Profile.jsx
@@ -1,28 +1,18 @@
 import React from 'react';
+import s from './Profile.module.sass';
+import MyPosts from './MyPosts/MyPosts';
+
 
 const Profile = () => {
 	return (
-		<div className='content'>
-				<div>
+		<div>
+				<div className={s.image}>
 					<img src="https://images.unsplash.com/photo-1517042271736-ddacb7a2263a?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1500&q=80" alt=""/>
 				</div>
 				<div>
 					ava + description
 				</div>
-				<div>
-					My posts
-					<div>
-						New posts
-					</div>
-					<div>
-						<div>
-							Post 1
-						</div>
-						<div>
-							Post 2
-						</div>
-					</div>
-				</div>
+				<MyPosts />
 			</div>
 	);
 }
