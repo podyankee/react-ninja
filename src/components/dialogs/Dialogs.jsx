92575ba6a0255ead90890afee8d59203718e8944
podyankee <podyankee@gmail.com>
2021-03-09 07:28:58 +0300

    Rout exact & Dialog props, lessons 22-23

--- src/components/dialogs/Dialogs.jsx
+++ src/components/dialogs/Dialogs.jsx
@@ -1,40 +1,60 @@
 import React from 'react';
 
 import s from './Dialog.module.sass';
+import {NavLink} from 'react-router-dom';
+
+const DialogItem = (props) => {
+	let path = "/dialogs/" + props.id;
+	return (
+		<div className={s.dialog + ' ' + s.active}>
+			<NavLink to={path}>{props.name}</NavLink>
+		</div>
+	)
+}
+
+const Message = (props) => {
+	return (
+		<div className={s.dialog}>
+			{props.message}
+		</div>
+	)
+}
+
+
 
 const Dialogs = (props) => {
 	return (
 		<div className={s.dialogs}>
 			<div className={s.dialogsItems}>
-				<div className={s.dialog + ' ' + s.active}>
-					Dimych
-				</div>
-				<div className={s.dialog}>
-					Andrey
-				</div>
-				<div className={s.dialog}>
-					Sveta
-				</div>
-				<div className={s.dialog}>
-					Sasha
-				</div>
-				<div className={s.dialog}>
-					Victor
-				</div>
-				<div className={s.dialog}>
-					Valera
-				</div>
+				<DialogItem
+					name="Dimych"
+					id = "1"
+				/>
+				<DialogItem
+					name="Andrey"
+					id = "4"
+				/>
+				<DialogItem
+					name="Sveta"
+					id = "3"
+				/>
+				<DialogItem
+					name="Sasha"
+					id = "4"
+				/>
+				<DialogItem
+					name="Victor"
+					id = "5"
+				/>
+				<DialogItem
+					name="Valera"
+					id = "6"
+				/>
 			</div>
 			<div className={s.messages}>
-				<div className={s.message}>
-					Hi
-				</div>
-				<div className={s.message}>
-					Hello
-				</div>
-				<div className={s.message}>
-					Yo
-				</div>
+				<Message message ="Hi" />
+				<Message message ="Hello" />
+				<Message message ="Yo" />
 			</div>
 		</div>
 
--- src/components/profile/Profile.jsx
+++ src/components/profile/Profile.jsx
@@ -1,17 +1,13 @@
 import React from 'react';
-import s from './Profile.module.sass';
+// import s from './Profile.module.sass';
 import MyPosts from './MyPosts/MyPosts';
+import ProfileInfo from './ProfileInfo/ProfileInfo';
 
 
 const Profile = () => {
 	return (
 		<div>
-				<div className={s.image}>
-					<img src="https://images.unsplash.com/photo-1517042271736-ddacb7a2263a?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1500&q=80" alt=""/>
-				</div>
-				<div>
-					ava + description
-				</div>
+				<ProfileInfo />
 				<MyPosts />
 			</div>
 	);
