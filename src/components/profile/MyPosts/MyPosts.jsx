7171ceaf9aba7aaff1ead188af4f7397ffeb630c
podyankee <podyankee@gmail.com>
2021-03-10 16:59:27 +0300

    onclick and ref

--- src/components/profile/MyPosts/MyPosts.jsx
+++ src/components/profile/MyPosts/MyPosts.jsx
@@ -10,16 +10,23 @@ const MyPosts = (props) => {
 	let postsElements = props.posts
 				.map(post => <Post message={post.message} likeCount={post.likeCount} />);
 
+	let newPostElement = React.createRef();
+
+	let addPost = () => {
+		let text = newPostElement.current.value;
+		alert(text);
+	}
+
 	return (
 
 				<div className={s.postsBlock}>
 					<h3>My posts</h3>
 					<div>
 						<div>
-							<textarea></textarea>
+							<textarea ref={newPostElement} ></textarea>
 						</div>
 						<div>
-							<button>Add post</button>
+							<button onClick={ addPost }>Add post</button>
 						</div>
 					</div>
 					<div className={s.posts}>
